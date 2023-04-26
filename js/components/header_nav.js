92e343ed94ec87524f1c1b08d8d483c4050d7a75
thikimyen-nguyen <nguyenthikimyen25@gmail.com>
2023-04-27 00:06:18 +0200

    add index.html

---
+++ js/components/header_nav.js
@@ -0,0 +1,4 @@
+const hamburger = document.querySelector(".hamburger");
+hamburger.innerHTML += `<div class="line"></div>
+                        <div class="line"></div>
+                        <div class="line"></div>`
---
+++ js/script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav_list');
+
+function showNavList() {
+    navList.style.display = "block";
+}
