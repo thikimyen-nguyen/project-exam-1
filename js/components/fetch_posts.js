8960e9bf3238f0b2f1a3760da2b12c51d22b5590
thikimyen-nguyen <nguyenthikimyen25@gmail.com>
2023-05-05 12:12:05 +0200

    fix recipes page

--- js/components/fetch_posts.js
+++ js/components/fetch_posts.js
@@ -10,22 +10,29 @@ async function getPosts() {
     return posts;
 }
 
-const recipesContainer = document.querySelector(".recipes_container");
-function createThumbnails(posts) {
-    for (let i = 0; i < posts.length; i++) {
-        const postImage = posts[i].jetpack_featured_media_url;
-        const postName = posts[i].title.rendered;
-        
-        recipesContainer.innerHTML += `<div>
-                                        <img src="${postImage}" alt="${postName}">
-                                        <h3>${postName}</h3>
-                                        </div>`
-    }
-}
-
 // main fetch
 async function createRecipesHTML() {
     const posts = await getPosts();
     createThumbnails(posts);
 }
 createRecipesHTML();
+
+// fetch first 10 posts
+const recipesContainer = document.querySelector(".recipes_container");
+function createThumbnails(posts) {
+    for (let i = 0; i < posts.length; i++) {
+        const postImage = posts[i].jetpack_featured_media_url;
+        const postName = posts[i].title.rendered;
+        const postID = posts[i].id;
+        console.log(postID);
+        
+        recipesContainer.innerHTML += ` <a href="detail.html?id=${postID}"  class="thumbnail"><div>
+                                        <img src="${postImage}" alt="${postName}">
+                                        <h2>${postName}</h2>
+                                        </div></a>`
+    }
+}
+
+// fetch next 10 posts
+const viewMoreButton = document.querySelector("#viewmore_button");
+
