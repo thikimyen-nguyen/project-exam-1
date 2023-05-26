784879d954055144cab3cb70b5a96db9e27a164f
thikimyen-nguyen <nguyenthikimyen25@gmail.com>
2023-05-26 10:45:58 +0200

    fix bugs

--- js/components/header_nav.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navList = document.querySelector('.nav_list');
-const hamburgerHtml = hamburger.innerHTML;
-hamburgerHtml += `<div class="line"></div>
-                        <div class="line"></div>
-                        <div class="line"></div>`
-hamburger.onclick = function showNavList() {
-    navList.style.display = "block";
-}
