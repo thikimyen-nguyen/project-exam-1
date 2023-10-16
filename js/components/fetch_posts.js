9017b02684e631879583661de7efa96a3652f7c1
thikimyen-nguyen <nguyenthikimyen25@gmail.com>
2023-10-16 13:28:28 +0200

    change API

--- js/components/fetch_posts.js
+++ js/components/fetch_posts.js
@@ -1,4 +1,4 @@
-const baseAPI = "http://34.82.129.237/";
+const baseAPI = "https://yen-nguyen.no/";
 const baseAllPosts = "/wp-json/wp/v2/posts";
 const postsURL = baseAPI + baseAllPosts;
 const loader = document.querySelector(".loader");
